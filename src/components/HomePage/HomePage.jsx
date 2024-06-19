af47f577ef713635d84c1d5991285e6fcc4391a6
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-06-19 15:33:10 -0500

    MUI cards added to Home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,6 +2,10 @@ import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import './HomePage.css';
 
 function HomePage() {
   const yarns = useSelector((store) => store.yarns.yarnInventory);
@@ -26,22 +30,46 @@ function HomePage() {
     <>
       <h1>Home</h1>
       <h2>Recent Yarns</h2>
-      <section>
+      <section className='yarn-container'>
         {yarns.map((yarn) => {
           return (
-            <div key={yarn.id} onClick={() => viewYarnDetails(yarn.id)}>
-              <p>{yarn.yarn_title}</p>
-            </div>
+            <Card
+              className='yarn-card'
+              variant='outlined'
+              sx={{ minWidth: 275 }}
+              style={{ backgroundColor: 'blanchedalmond' }}
+              key={yarn.id}
+              onClick={() => viewYarnDetails(yarn.id)}
+            >
+              <CardContent className='card-content'>
+                <img style={{ width: '15rem', height: '20rem' }} src={yarn.image} alt={yarn.yarn_title} />
+                <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+                  {yarn.name}: {yarn.yarn_title}
+                </Typography>
+              </CardContent>
+            </Card>
           );
         })}
       </section>
       <h2>Recent Patterns</h2>
-      <section>
+      <section className='pattern-container'>
         {patterns.map((pattern) => {
           return (
-            <div key={pattern.id} onClick={() => viewPatternDetails(pattern.id)}>
-              <p>{pattern.pattern_title}</p>
-            </div>
+            <Card
+              className='yarn-card'
+              variant='outlined'
+              sx={{ minWidth: 275 }}
+              style={{ backgroundColor: 'blanchedalmond' }}
+              key={pattern.id}
+              onClick={() => viewPatternDetails(pattern.id)}
+            >
+              <CardContent className='card-content'>
+                <img style={{ width: '15rem', height: '20rem' }} src={pattern.image} alt={pattern.pattern_title} />
+                <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+                  {pattern.name}: {pattern.pattern_title}
+                </Typography>
+              </CardContent>
+            </Card>
           );
         })}
       </section>
